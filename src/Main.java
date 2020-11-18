e4df7e7a8fd21b0ab22bd2dcea7b455af14d6831
Alexey Andreev <dark-cross@rambler.ru>
2020-11-18 23:51:56 +0300

    'Bonus'

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int balance = 5;
+        int balance = 550;
 
-        int amount;
-        amount = 1001;
+        int amount = 100;
 
         int limit;
         limit = 1000;
@@ -13,16 +12,15 @@ public class Main {
         int bonus;
 
         if (amount > limit) {
-            bonus = balance + amount + amount/100;
-        }
-        else { bonus = balance + amount;
+            bonus = balance + amount + amount / 100;
+        } else {
+            bonus = balance + amount;
         }
         System.out.println(bonus);
-        }
+    }
 
 
-
-        }
+}
 
 
 
